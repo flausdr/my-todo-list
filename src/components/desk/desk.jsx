ca286300bd3a033a3a1f6923a6ec4ae57f00d557
Flaus-react-pc <mrychkov1302@gmail.com>
2022-12-29 19:58:12 +0200

    adding media screen

--- src/components/desk/desk.jsx
+++ src/components/desk/desk.jsx
@@ -4,6 +4,7 @@ import { addCard } from '../cardSlice/cardSlice';
 import Item from '../list-item/list-item';
 import { useEffect } from 'react';
 import Sortable from 'sortablejs';
+import { ReactComponent as Plus } from '../../assets/img/plus.svg';
 
 import './desk.scss';
 
@@ -16,9 +17,10 @@ const Desk = () => {
         const el = document.getElementById('items')
         new Sortable(el, {
             animation: 150,
-            ghostClass: 'blue-background-class'
+            ghostClass: 'blue-background-class',
+            touchStartThreshold: 3
         })
-    })
+    }, [])
     return (
         <div className="desk">
             <ul className='list-desk' id='items'>
@@ -30,9 +32,7 @@ const Desk = () => {
             </ul>
             <div className='add-item' onClick={() => dispatch(addCard({id: randomId, obj: {id: randomId, text: ''}}))} >
                 <span className="add-new-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                    </svg>
+                    <Plus />
                 </span>
                 <h3 className='add-text'>Add new card</h3>
             </div>
--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { removeItemCard } from '../cardSlice/cardSlice';
+import { ReactComponent as Close } from '../../assets/img/close.svg';
 
 const Item = (state) => {
     const [text, setText] = useState(''),
@@ -10,9 +11,7 @@ const Item = (state) => {
         <li>
             <textarea type="text" placeholder='New item..' value={text} onChange={(e) => setText(e.target.value)}/>
             <span className='close-item' onClick={() => dispatch(removeItemCard({cardId: state.cardId, itemId: state.id}))}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                    <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
-                </svg>
+                <Close />
             </span>
         </li>
     )
